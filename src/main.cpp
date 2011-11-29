8529635b443d46284d4aa03bdb2c17f14ce8f2a0
pierrem <pierrem@3c0a9016-307c-449d-a262-9f71d4c15c50>
2011-11-29 15:37:12 +0000

    F - unpleasant #defines
    git-svn-id: http://xvm-2-183.ghst.net/svn/L/fonzie@78 3c0a9016-307c-449d-a262-9f71d4c15c50

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@
 #include "composer.h"
 
 
-#define WITH_TESSERACT
+//#define WITH_TESSERACT
 #ifdef WITH_TESSERACT
 #include "ocr.h"
 #endif
@@ -44,12 +44,12 @@
 #include <QFile>
 #include <QDir>
 
-#define WITH_CAPTURE
+//#define WITH_CAPTURE
 #ifdef WITH_CAPTURE
 #include "orm.h"
 #endif
 
-#define WITH_BOOK_SCANNER
+//#define WITH_BOOK_SCANNER
 #ifdef WITH_BOOK_SCANNER
 #include "bookscanner.h"
 #endif
